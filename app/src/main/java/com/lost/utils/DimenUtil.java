f0cddb4d2f91c1a302ed7a5c169d8760dff1515a
lichaofeng <lichaofengfcl@163.com>
2017-03-15 21:46:11 +0800

    修改

--- app/src/main/java/com/lost/utils/DimenUtil.java
+++ app/src/main/java/com/lost/utils/DimenUtil.java
@@ -12,21 +12,21 @@ public class DimenUtil {
 
     public static int px2dp(Context context, int pxValue) {
         float density = context.getResources().getDisplayMetrics().density;
-        return (int) (pxValue * density + 0.5f);
+        return (int) (pxValue / density + 0.5f);
     }
 
     public static int dp2px(Context context, float dpValue) {
         float density = context.getResources().getDisplayMetrics().density;
-        return (int) (dpValue / density + 0.5f);
+        return (int) (dpValue * density + 0.5f);
     }
 
     public static int px2sp(Context context, int pxValue) {
         float scaleDensity = context.getResources().getDisplayMetrics().scaledDensity;
-        return (int) (pxValue * scaleDensity + 0.5f);
+        return (int) (pxValue / scaleDensity + 0.5f);
     }
 
     public static int sp2px(Context context, float spValue) {
         float scaleDensity = context.getResources().getDisplayMetrics().scaledDensity;
-        return (int) (spValue / scaleDensity + 0.5f);
+        return (int) (spValue * scaleDensity + 0.5f);
     }
 }
